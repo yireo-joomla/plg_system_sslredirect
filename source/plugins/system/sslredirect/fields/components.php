4986d1c57e3b5de63521eef830052489f81af285
Jisse Reitsma <info@yireo.com>
2016-05-12 13:17:57 +0200

    Add "None" option to Menu Item fields

--- source/plugins/system/sslredirect/fields/components.php
+++ source/plugins/system/sslredirect/fields/components.php
@@ -2,57 +2,61 @@
 /**
  * Joomla! System plugin for SSL redirection
  *
- * @author Yireo (info@yireo.com)
- * @package Joomla!
- * @copyright Copyright 2015
- * @license GNU Public License
- * @link http://www.yireo.com
+ * @author    Yireo (info@yireo.com)
+ * @package   Joomla!
+ * @copyright Copyright 2016
+ * @license   GNU Public License
+ * @link      https://www.yireo.com
  */
 
 // Check to ensure this file is included in Joomla!
-defined('_JEXEC') or die( 'Restricted access' );
+defined('_JEXEC') or die('Restricted access');
 
 // Import classes
 jimport('joomla.html.html');
 jimport('joomla.access.access');
 jimport('joomla.form.formfield');
-include_once JPATH_LIBRARIES.'/joomla/form/fields/list.php';
+
+include_once JPATH_LIBRARIES . '/joomla/form/fields/list.php';
 
 /**
  * Form Field-class for selecting a component
  */
 class JFormFieldComponents extends JFormFieldList
 {
-    /*
-     * Form field type
-     */
-    public $type = 'Components';
+	/*
+	 * Form field type
+	 */
+	public $type = 'Components';
 
-    /*
-     * Method to construct the HTML of this element
-     *
-     * @param null
-     * @return string
-     */
-    protected function getInput()
-    {
-        $name = $this->name.'[]';
-        $value = $this->value;
-        $db = JFactory::getDBO();
+	/*
+	 * Method to construct the HTML of this element
+	 *
+	 * @return string
+	 */
+	protected function getInput()
+	{
+		$name = $this->name . '[]';
+		$value = $this->value;
+		$db = JFactory::getDbo();
 
-        // load the list of components
-        $query = 'SELECT * FROM `#__extensions` WHERE `type`="component" AND `enabled`=1';
-        $db->setQuery( $query );
-        $components = $db->loadObjectList();
+		// Load the list of components
+		$query = 'SELECT * FROM `#__extensions` WHERE `type`="component" AND `enabled`=1';
+		$db->setQuery($query);
+		$components = $db->loadObjectList();
 
-        $options = array();
-        foreach ($components as $component) {
-            $options[] = JHTML::_('select.option',  $component->element, $component->name.' ['.$component->element.']', 'value', 'text');
-        }
+		$options = array();
+		
+		foreach ($components as $component)
+		{
+			$options[] = JHTML::_('select.option', $component->element, $component->name . ' [' . $component->element . ']', 'value', 'text');
+		}
+		
 		$options = array_merge(parent::getOptions(), $options);
 
-        $size = (count($options) > 12) ? 12 : count($options);
-        $attribs = 'class="inputbox" multiple="multiple" size="'.$size.'"';
-        return JHTML::_('select.genericlist',  $options, $name, $attribs, 'value', 'text', $value, $name);
-    }
+		$size = (count($options) > 12) ? 12 : count($options);
+		$attribs = 'class="inputbox" multiple="multiple" size="' . $size . '"';
+
+		return JHTML::_('select.genericlist', $options, $name, $attribs, 'value', 'text', $value, $name);
+	}
 }
--- source/plugins/system/sslredirect/fields/menuitems.php
+++ source/plugins/system/sslredirect/fields/menuitems.php
@@ -2,52 +2,58 @@
 /**
  * Joomla! System plugin for SSL redirection
  *
- * @author Yireo (info@yireo.com)
- * @package Joomla!
- * @copyright Copyright 2015
- * @license GNU Public License
- * @link http://www.yireo.com
+ * @author    Yireo (info@yireo.com)
+ * @package   Joomla!
+ * @copyright Copyright 2016
+ * @license   GNU Public License
+ * @link      https://www.yireo.com
  */
 
 // Check to ensure this file is included in Joomla!
-defined('_JEXEC') or die( 'Restricted access' );
+defined('_JEXEC') or die('Restricted access');
 
 // Import classes
 jimport('joomla.html.html');
 jimport('joomla.access.access');
 jimport('joomla.form.formfield');
 
+include_once JPATH_LIBRARIES . '/joomla/form/fields/list.php';
+
 /**
  * Form Field-class for selecting multiple menu-items
  */
-class JFormFieldMenuItems extends JFormField
+class JFormFieldMenuItems extends JFormFieldList
 {
-    public $type = 'MenuItems';
+	public $type = 'MenuItems';
 
-    /*
-     * Method to construct the HTML of this element
-     *
-     * @param null
-     * @return string
-     */
-    protected function getInput()
-    {
-        $name = $this->name.'[]';
-        $value = $this->value;
-        $db = JFactory::getDBO();
+	/*
+	 * Method to construct the HTML of this element
+	 *
+	 * @return string
+	 */
+	protected function getInput()
+	{
+		$name  = $this->name . '[]';
+		$value = $this->value;
+		$db    = JFactory::getDbo();
 
-        // load the list of components
-        $query = 'SELECT * FROM `#__menu` WHERE `client_id`="0"';
-        $db->setQuery( $query );
-        $menuitems = $db->loadObjectList();
+		// Load the list of components
+		$query = 'SELECT * FROM `#__menu` WHERE `client_id`="0"';
+		$db->setQuery($query);
+		$menuitems = $db->loadObjectList();
 
-        $options = array();
-        foreach ($menuitems as $menuitem) {
-            $options[] = JHTML::_('select.option',  $menuitem->id, $menuitem->title.' ['.$menuitem->id.']', 'value', 'text');
-        }
+		$options = array();
 
-        $size = (count($options) > 12) ? 12 : count($options);
-        $attribs = 'class="inputbox" multiple="multiple" size="'.$size.'"';
-        return JHTML::_('select.genericlist',  $options, $name, $attribs, 'value', 'text', $value, $name);
-    }
+		$options = array_merge(parent::getOptions(), $options);
+
+		foreach ($menuitems as $menuitem)
+		{
+			$options[] = JHTML::_('select.option', $menuitem->id, $menuitem->title . ' [' . $menuitem->id . ']', 'value', 'text');
+		}
+
+		$size    = (count($options) > 12) ? 12 : count($options);
+		$attribs = 'class="inputbox" multiple="multiple" size="' . $size . '"';
+
+		return JHTML::_('select.genericlist', $options, $name, $attribs, 'value', 'text', $value, $name);
+	}
 }
